3fc6f24f2f541731deb3dfccfdfc12ca32a4ac51
Kadotchi <kadotchi724@gmail.com>
2023-08-20 17:05:52 +0900

    プロジェクトの作成

---
+++ src/main/java/kadotchi/sample/SpringAwsSampleApplication.java
@@ -0,0 +1,24 @@
+package kadotchi.sample;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsSampleApplication implements ApplicationRunner {
+
+    @Value("${process.type}")
+    private String processType;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsSampleApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        System.out.println(processType);
+        System.out.println("Hello World!!");
+    }
+}
---
+++ src/test/java/kadotchi/sample/SpringAwsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kadotchi.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsSampleApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
