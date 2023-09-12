9d4b3b79b6677f07d8f7bf00108619cb068877d7
Kadotchi <kadotchi724@gmail.com>
2023-09-13 06:32:35 +0900

    初期画面を表示するハンドラメソッド

--- src/main/java/kadotchi/sample/SampleController.java
+++ src/main/java/kadotchi/sample/SampleController.java
@@ -1,8 +1,13 @@
 package kadotchi.sample;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class SampleController {
@@ -12,6 +17,15 @@ public class SampleController {
     public String displaySample(Model model){
         model.addAttribute("fullName", "埼玉次郎");
         return "sample";
+    }
+
+    @GetMapping("/reservation/display-form")
+    public String displayForm(@RequestParam String trainingId, Model model){
+        ReservationInput reservationInput = new ReservationInput();
+        reservationInput.setTrainingId(trainingId);
+        reservationInput.setStudentTypeCode("EMPLOYEE");
+        model.addAttribute("reservationInput", reservationInput);
+        return "reservation/reservationForm";
     }
 
 }
